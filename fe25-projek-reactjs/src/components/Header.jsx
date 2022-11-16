d394d642ac6a20ca619c3b6937c5456b03f94ef4
luthfi <luthfi2001@gmail.com>
2022-11-16 11:58:06 +0700

    tambahin git commit

--- fe25-projek-reactjs/src/components/Header.jsx
+++ fe25-projek-reactjs/src/components/Header.jsx
@@ -1,13 +1,65 @@
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon,
+  MDBCollapse,
+  MDBBtn
+} from "mdb-react-ui-kit";
+import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 const Header = () => {
+  const [showBasic, setShowBasic] = useState(false);
   return (
-    <div className="d-flex">
-      <h1>Header</h1>
-      <Link to="/">Home</Link>
+    <header>
+    <MDBNavbar expand='lg' light bgColor='white'>
+      <MDBContainer fluid>
+        <MDBNavbarToggler
+          onClick={() => setShowBasic(!showBasic)}
+          aria-controls='navbarExample01'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <MDBIcon fas icon='bars' />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav right className='mb-2 mb-lg-1'>
 
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>About</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+
+    <div
+      className='p-5 text-center bg-image'
+      style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: '400px' }}
+    >
+      <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+        <div className='d-flex justify-content-center align-items-center h-100'>
+          <div className='text-white'>
+            <h1 className='mb-3'>Headsdsg</h1>
+            <h4 className='mb-3'>Subheading</h4>
+            <MDBBtn tag="a" outline size="lg">
+              Call to action
+            </MDBBtn>
+          </div>
+        </div>
+      </div>
     </div>
+  </header>
   );
 };
 
