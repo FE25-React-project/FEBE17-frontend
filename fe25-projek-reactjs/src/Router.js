0193c253a5f9dcb8e30b002ec95d6d6f4d05d1aa
luthfi <luthfi2001@gmail.com>
2022-11-16 10:29:52 +0700

    tambahin router

--- fe25-projek-reactjs/src/Router.js
+++ fe25-projek-reactjs/src/Router.js
@@ -1,7 +1,13 @@
+import  { Routes,Route } from 'react-router-dom'
+import Loginpage from './components/Loginpage';
+import Registerpage from './components/Registerpage';
 const Routerapp = () => {
     return ( 
         <div>
-            dari router
+             <Routes>
+                <Route path='/login' element={<Loginpage/>} />
+                <Route path='/register' element={<Registerpage/>} />
+             </Routes>
         </div>
      );
 }
